a66ede3c31676f4edcfeb28065cbeb157b6b7f72
Levi Schubert <me@levischubert.com>
2018-04-24 13:08:04 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const DragDropManager = Object.create(null, {
+	init: {
+	  value: () => {
+		const stages = document.querySelectorAll(".stage")
+  
+		stages.forEach(stage => {
+		  // Gain reference of item being dragged
+		  stage.ondragstart = e => {
+			e.dataTransfer.setData("text", e.target.classList)
+		  }
+		})
+  
+  
+		const targets = document.querySelectorAll(".target")
+  
+		targets.forEach(target => {
+		  // Dragover not supported by default. Turn that off.
+		  target.ondragover = e => e.preventDefault()
+  
+		  target.ondrop = e => {
+			// Enabled dropping on targets
+			e.preventDefault()
+  
+			// Determine what's being dropped
+			const data = e.dataTransfer.getData("text")
+  
+			// Append card to target component as child
+			// TODO: This should only happen if the target has no children nodes
+			// TODO: This should not happen if the target is another stage card
+			if(!(e.target.hasChildNodes() || e.target.classList.contains("stage"))){
+				e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+			}else if(e.target.classList.contains("home")){
+				e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+			}
+		  }
+		})
+	  }
+	}
+  })
+  
+  DragDropManager.init()
